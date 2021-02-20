37443deef9218ffb7d3ad87a9dab38cffbffaea2
Osman Sekerlen <1279406+pegasuspect@users.noreply.github.com>
2021-02-20 13:55:54 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,9 @@ isNameDeclared=$(declare -F stash)
 
 if [[ -n "$isNameDeclared" ]]; then # -n: nonzero length check
 	echo "Your stash is ready to use. Now, type: stash";
-elif [[ -f $HOME/.bash_profile ]]; then
-	cat $(pwd)/stash_function.sh >> $HOME/.bash_profile
-	echo "Successfully installed stash function to your $HOME/.bash_profile";
 else
-	cat $(pwd)/stash_function.sh > $HOME/.bash_profile;
-	echo "Successfully installed! Now, type: stash";
+	cat $(pwd)/stash_function.sh >> $HOME/.bash_profile
+	echo "Stash function is installed to your $HOME/.bash_profile";
+	echo "Now, type: stash";
 fi
 unset isNameDeclared;
